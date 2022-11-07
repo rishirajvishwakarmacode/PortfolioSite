73cb07cf6b5d6d28764e4c02b4d4ed77cb1eac63
Rishi Raj Vishwakarma <coderishiraj@gmail.com>
2022-11-08 00:26:46 +0530

    Completed Projects and design of Contact section issues : same as previous to do : Complete the messaging part of Contacts

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,9 +1,39 @@
 import React from 'react'
+import {AiOutlineMail} from 'react-icons/ai';
+import {BsWhatsapp} from 'react-icons/bs';
+import '../Contact/Contact.css';
+
 
 const Contact = () => {
   return (
     <section id='contact'>
-        Contact
+        <h5>Get in Touch</h5>
+        <h2>Contact Me</h2>
+        <div className="container contact_container">
+      
+          <div className="contact_options">
+            <article className="contact_option">
+              <AiOutlineMail className='contact_icon'/>
+              <h4>Email</h4>
+              <h5>coderishiraj@gmail.com</h5>
+              <a href="mailto:coderishiraj@gmail.com" target='_blank'>Send a Mail</a>
+            </article>
+
+            <article className="contact_option">
+              <BsWhatsapp className='contact_icon'/>
+              <h4>WhatsApp</h4>
+              <h5>+91 78*******4</h5>
+              <a href="https://api.whatsapp.com/send?phone=+917806099514" target='_blank'>Text Me !!</a>
+            </article>
+          </div>
+
+          <form action="">
+            <input type="text" name="name" placeholder="Your Name Please" required />
+            <input type="email" name="email" placeholder="Enter your Email address" required />
+            <textarea name="message" rows="7" placeholder="Tell me! How can I help you?" required />
+            <button type='submit' className='btn btn-primary'>Send Message</button>
+          </form>
+        </div>
     </section>
   )
 }
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,9 +1,91 @@
 import React from 'react'
+import '../Projects/Projects.css';
+import img1 from '../../assets/portfolio1.jpg';
+
+
 
 const Projects = () => {
   return (
     <section id="projects">
-        Projects
+        <h5>My Recent Work</h5>
+        <h2>Projects</h2>
+
+        <div className='container projects_container'>
+         <article className='portfolio_item'>
+            <div className='project_image'>
+              <img src={img1} alt="project img" />
+            </div>
+            <h3>This is Project Title</h3>
+            <div className='portfolio_item_cta'>
+              <a href='https://www.github.com' className='btn btn-primary' target='_blank'>Github</a>
+              <a href='https://www.bizupeach.com' className='btn' target='_blank'>Demo</a>
+            </div>
+          </article>
+
+          <article className='portfolio_item'>
+            <div className='project_image'>
+              <img src={img1} alt="project img" />
+            </div>
+            <h3>This is Project Title</h3>
+            <div className='portfolio_item_cta'>
+              <a href='https://www.github.com' className='btn btn-primary' target='_blank'>Github</a>
+              <a href='https://www.bizupeach.com' className='btn' target='_blank'>Demo</a>
+            </div>
+          </article>
+
+          <article className='portfolio_item'>
+            <div className='project_image'>
+              <img src={img1} alt="project img" />
+            </div>
+            <h3>This is Project Title</h3>
+            <div className='portfolio_item_cta'>
+              <a href='https://www.github.com' className='btn btn-primary' target='_blank'>Github</a>
+              <a href='https://www.bizupeach.com' className='btn' target='_blank'>Demo</a>
+            </div>
+          </article>
+
+        </div>
+
+        <br></br>
+
+        <h2>Ongoing Projects</h2>
+
+        <div className='container projects_container'>
+         <article className='portfolio_item'>
+            <div className='project_image'>
+              <img src={img1} alt="project img" />
+            </div>
+            <h3>This is Project Title</h3>
+            <div className='portfolio_item_cta'>
+              <a href='https://www.github.com' className='btn btn-primary' target='_blank'>Github</a>
+              <a href='https://www.bizupeach.com' className='btn' target='_blank'>Join Project</a>
+            </div>
+          </article>
+
+          <article className='portfolio_item'>
+            <div className='project_image'>
+              <img src={img1} alt="project img" />
+            </div>
+            <h3>This is Project Title</h3>
+            <div className='portfolio_item_cta'>
+              <a href='https://www.github.com' className='btn btn-primary' target='_blank'>Github</a>
+              <a href='https://www.bizupeach.com' className='btn' target='_blank'>Join Project</a>
+            </div>
+          </article>
+
+          <article className='portfolio_item'>
+            <div className='project_image'>
+              <img src={img1} alt="project img" />
+            </div>
+            <h3>This is Project Title</h3>
+            <div className='portfolio_item_cta'>
+              <a href='https://www.github.com' className='btn btn-primary' target='_blank'>Github</a>
+              <a href='https://www.bizupeach.com' className='btn' target='_blank'>Join Project</a>
+            </div>
+          </article>
+          
+        </div>
+        
     </section>
   )
 }
